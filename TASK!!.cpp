b045c81b7d240e2c1c3b667119b01340905269fc
dhruv200713 <85639619+dhruv200713@users.noreply.github.com>
2021-06-09 22:48:15 +0530

    first task!!

---
+++ TASK!!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout<<"\n\n\n\tSize of char: 1 Byte";
+	cout<<"\n\tSize of int: 4 Bytes";
+	cout<<"\n\tSize of float: 4 Bytes";
+	cout<<"\n\tSize of double: 8 Bytes";
+	cout<<"\n\n\n";
+	return 0;
+	
+}
+//char 1
+//int 4 
+//float 4
+//double 8
