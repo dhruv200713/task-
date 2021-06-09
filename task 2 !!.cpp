52edd2fd57fdd20a4bbb5e4a82507bdb940e91d4
dhruv200713 <85639619+dhruv200713@users.noreply.github.com>
2021-06-09 23:12:35 +0530

    TASK 2 !!!

---
+++ task 2 !!.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int x;
+	int y;
+	
+	
+	cout<<"\n\tENTER TWO NUMBERS FOR ADDITION : \n\t";
+	cin>>x;
+	cin>>y;
+	int z = x + y;
+	cout<<"\tTHE SUM OF THE TWO NUMBERS IS : "<<z;
+	
+}
